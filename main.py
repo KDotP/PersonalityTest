c6103d76a148098d3997babf850f5b9d6f52e4c3
KDotP <kpersoff5@gmail.com>
2026-04-17 17:13:05 -0600

    added arcs

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random, socket
+
 basic_score = 0
 question_num = 0
 
@@ -10,8 +12,8 @@ def Display_Ending(ending_name, ending_lore):
         print(line)
     print("-----------------------------------")
     print("Press enter if you would like to take the test again.", end=" ")
-    input()
-    main()
+    input() # Wait for enter to continue
+    main() # Loop back to start, leaving previous loops in memeory (bad)
 
 # This is the basic question/answer function
 # Will be reused by wrappers that expand upon the basic function
@@ -43,6 +45,16 @@ def Targeted_Choice(question, answer1, answer2, answer3, target_answer):
         return 1
     else:
         return 0
+    
+def Free_Write(question):
+    global question_num
+    question_num += 1
+    print("")
+    print("----------------------------------")
+    print(f"Question {question_num}: {question}")
+    print("----------------------------------")
+    print("This is a free write question. Your answer will be shared to a server and judged to determine the most similar personaility type.") # lol no it won't
+    return input("Your answer: ")
 
 def Display_Beginning():
     print("Welcome to the world's only accurate personality test. This will be a grueling and difficult test, but if you can make it through, you will be provided with unprecidented incite into your very heart and soul.")
@@ -90,6 +102,49 @@ def main():
     if (murder_suspect == 2):
         murder_suspect += Targeted_Choice("Interesting. Are you familiar with this?", "With what? You didn't show me anything!", "Oh hey, that's mine!", "Why are you holding a gun?", 2)
     if (murder_suspect == 3):
-        murder_suspect += Targeted_Choice("")
+        murder_suspect += Targeted_Choice("And are these your fingerprints?", "Doesn't look like it", "I actually can't tell through text", "Could be", 3)
+    if (murder_suspect == 4):
+        Choice("Can you put your hands behind your back?", "Yeah, why?", "What?", "Hold on")
+        Display_Ending("MURD - Arrested", [
+            "For the crime of murdering one Mr. Callahan on the night of May 9th, 2023 at Mukilteo Lighthouse in Seattle, Washington, you have been arrested.",
+            "You have the right to remain silent. You have the right to an attorney. If you cannot afford one, one will be provided for you.",
+            "You're probably not going to see sunlight for a while, so get one last look."
+        ])
 
+    # Jump here if any previous answer is "incorrect"
+    Choice("What is your favorite artistic medium?", "Painting", "Film", "Games")
+    perfectionist = Choice("How many times do you reread a text or message before sending it?", "Once, if that", "A handful of times", "I could spend all day rewriting a single sentence")
+    liar = Targeted_Choice("Have you told the truth for this entire test?", "I lied one the first question to see the other endings", "I lied because I'm afraid you're going to sell my data to advertisers", "Nope, all truth!", 2)
+
+    # The unlucky ending, 1 in 20 chance
+    if (random.random() > 0.95):
+        Display_Ending("UNLY - Unlucky", [
+            "Fun fact: after every question, there is a 1 in 1000 chance that you will be given this ending.",
+            "Sorry, but being unlucky is just part of who you are. Embrace it, and maybe one day you'll get lucky!",
+            "Not likely though.",
+            "Sorry."
+            "You will have to take the test again if you want to see your real ending, but hey, at least you got to see this one!"
+        ])
+
+    Choice("What is your favorite season?", "Spring", "Summer", "Fall")
+
+    # IP check (no answers recorded)
+    try:
+        ip_address = socket.gethostbyname(socket.gethostname())
+        Choice(f"Is {ip_address} your IP address?", "Yes", "Yes?", "Yes, why?")
+    except:
+        Choice("Did something pop up on your screen just now?", "No, everything's fine", "I think so?", "Yes, I got a virus")
+
+    liar += Targeted_Choice("How long do you think you could pretend to be someone else in casual conversation?", "I could keep it up for hours", "I could try, but my heart wouldn't be in it", "I have no interest in impersonating someone else", 1)
+    awkward_score += Targeted_Choice("How often do you think you blink compared to the average person?", "Probably a bit less", "...", "Probably a bit more", 2)
+    if (awkward_score >= 3):
+        Display_Ending("AKWD - Awkward Test Taker", [
+            "Look.",
+            "Maybe we got off on the wrong foot. Maybe you were forced to take this test against your will. But at least I'm out here trying.",
+            "You're giving me nothing here. What, you thought that answering \"...\" at every available oppertunity was going to get you some special ending?",
+            "Congrats, you got it! Now we're both just sitting here, a little uncomfortable, not sure what to say. I hope you're happy with yourself."
+        ])
+    procrastinator = Targeted_Choice("You are given a difficult task you must complete within a month, how do you approach it?", "Get it done as fast as possible", "Get a bit done one day at a time", "Get everything done in the last week", 3)
+    agression += Targeted_Choice("Somebow steals your lunch from the office fridge, how do you react?", "I poision the food so when they steal it again, they get their just reward", "I get really mad but don't say anything", "I just let it go, it's not worth the conflict", 1)
+               
 main()
