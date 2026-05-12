511a1a55bebcaf60a9c6773661c1383b0bc9992e
KDotP <kpersoff5@gmail.com>
2026-05-11 18:47:03 -0600

    Replaced if tree with a dictionary (even if I didn't want to), restructured references to be on a seperate file to improve readability (the only thing that matters)

---
+++ REFERENCE.py
@@ -0,0 +1,273 @@
+# This file does not contain any code, but contains all the sprites for the dungeon crawler minigame
+# This is done to reduce file size and make editing easier
+# Credits for each ASCII art image is above the variable (except for the dictionary at the end)
+
+# Sourced via https://www.asciiart.eu/art/23319246a6009130, author Tim Spicer (Darkstar), modified
+CORRIDOR_SPLIT = r"""
+ _____________________________________________
+|.'',                                     ,''.|
+|.'.'',                                 ,''.'.|
+|.'.'.'',                             ,''.'.'.|
+|.'.'.'.'',                         ,''.'.'.'.|
+|.'.'.'.'.|                         |.'.'.'.'.|
+|.'.'.'.'.|=========================|.'.'.'.'.|
+|.'.'.'.'.|:::|::::::::|::::::::|:::|.'.'.'.'.|
+|.'.'.'.'.|---|--------|--------|---|.'.'.'.'.|
+|.'.'.'.'.|:::|::::::::|::::::::|:::|.'.'.'.'.|
+|,',',',',|---|--------|--------|---|,',',',',|
+|.'.'.'.'.|:::|::::::::|::::::::|:::|.'.'.'.'.|
+|.'.'.'.'.|---|--------|--------|---|.'.'.'.'.|
+|.'.'.'.'.|=========================|.'.'.'.'.|
+|.'.'.'.'.|%%%%%%%%%%%%%%%%%%%%%%%%%|.'.'.'.'.|
+|.'.'.'.','       /%%%%%%%%%\       ','.'.'.'.|
+|.'.'.','        /%%%%%%%%%%%\        ','.'.'.|
+|.'.','         /%%%%%%%%%%%%%\         ','.'.|
+|.','          /%%%%%%%%%%%%%%%\          ','.|
+|;____________/%%%%%%%%%%%%%%%%%\____________;|
+"""
+
+# Sourced via https://www.asciiart.eu/art/619e9b9cbc49d3c1, author Shanaka Dias
+DRAGON_REELING = r"""
+             __                  __
+            ( _)                ( _)
+           / / \\              / /\_\_
+          / /   \\            / / | \ \
+         / /     \\          / /  |\ \ \
+        /  /   ,  \ ,       / /   /|  \ \
+       /  /    |\_ /|      / /   / \   \_\
+      /  /  |\/ _ '_| \   / /   /   \    \\
+     |  /   |/  0 \0\    / |    |    \    \\
+     |    |\|      \_\_ /  /    |     \    \\
+     |  | |/    \.\ o\o)  /      \     |    \\
+     \    |     /\\`v-v  /        |    |     \\
+      | \/    /_| \\_|  /         |    | \    \\
+      | |    /__/_ `-` /   _____  |    |  \    \\
+      \|    [__]  \_/  |_________  \   |   \    ()
+       /    [___] (    \         \  |\ |   |   //
+      |    [___]                  |\| \|   /  |/
+     /|    [____]                  \  |/\ / / ||
+    (  \   [____ /     ) _\      \  \    \| | ||
+     \  \  [_____|    / /     __/    \   / / //
+     |   \ [_____/   / /        \    |   \/ //
+     |   /  '----|   /=\____   _/    |   / //
+  __ /  /        |  /   ___/  _/\    \  | ||
+ (/-(/-\)       /   \  (/\/\)/  |    /  | /
+               (/\/\)           /   /   //
+                      _________/   /    /
+                     \____________/    (
+"""
+
+# Sourced via https://www.asciiart.eu/art/376a59d7692b8079, author Shanaka Dias
+DRAGON_STATIC = r"""
+ <>=======() 
+(/\___   /|\\          ()==========<>_
+      \_/ | \\        //|\   ______/ \)
+        \_|  \\      // | \_/
+          \|\/|\_   //  /\/
+           (oo)\ \_//  /
+          //_/\_\/ /  |
+         @@/  |=\  \  |
+              \_=\_ \ |
+                \==\ \|\_
+             __(\===\(  )\
+            (((~) __(_/   |
+                 (((~) \  /
+                 ______/ /
+                 '------'
+"""
+
+# Sourced via https://www.asciiart.eu/art/1833b5c2ffc7fafc, author Shanaka Dias, modified
+DRAGON_CHARGING = r"""
+          /                            )
+          (                             |\
+         /|                              \\
+        //                                \\
+       ///                                 \|
+      /( \                                  )\
+      \\  \_                               //)
+       \\  :\__                           ///
+        \\     )                         // \
+         \\:  /                         // |/
+          \\ / \                       //  \
+           /)   \   ___..-'           (|  \_|
+          //     /   _.'              \ \  \
+         /|       \ \________          \ | /
+        (| _ _  __/          '-.       ) /.'
+         \\ .  '-.__            \_    / / \
+          \\_'.     > --._ '.     \  / / /
+           \ \      \     \  \     .' /.'
+            \ \  '._ /     \ )    / .' |
+             \ \_     \_   |    .'_/ __/
+              \  \      \_ |   / /  _/ \_
+               \  \       / _.' /  /     \
+               \   |     /.'   / .'       '-,_
+                \   \  .'   _.'_/             \
+   /\    /\      ) ___(    /_.'           \    |
+  | _\__// \    (.'      _/               |    |
+  \/_  __  /--'`    ,                   __/    /
+  ( b) /b)  \  '.   :            \___.-'_/ \__/
+  /:/:  ,     ) :        (      /_.'__/-'|_ _ /
+ /:/: __/\ >  __,_.----.__\    /        (/(/(/
+(_(,_/V .'/--'    _/  __/ |   /
+ VvvV  //`    _.-' _.'     \   \
+   n_n//     (((/->/        |   /
+   '--'         ~='          \  |
+                              | |_,,,
+                              \  \  /
+                               '.__)
+"""
+
+# Sourced via https://www.asciiart.eu/art/627363361eda1a20, author unknown
+UNICORN_STATIC = r"""
+                              /
+                   __       //
+                   -\= \=\ //
+                 --=_\=---//=--
+               -_==/  \/ //\/--
+                ==/   /O   O\==--
+   _ _ _ _     /_/    \  ]  /--
+  /\ ( (- \    /       ] ] ]==-
+ (\ _\_\_\-\__/     \  (,_,)--
+(\_/                 \     \-
+\/      /       (   ( \  ] /)
+/      (         \   \_ \./ )
+(       \         \      )  \
+(       /\_ _ _ _ /---/ /\_  \
+ \     / \     / ____/ /   \  \
+  (   /   )   / /  /__ )   (  )
+  (  )   / __/ '---`       / /
+  \  /   \ \             _/ /
+  ] ]     )_\_         /__\/
+  /_\     ]___\
+ (___)
+"""
+
+# Sourced via https://www.asciiart.eu/art/b73d333aa387ff07, author Tua Xiong, modified
+UNICORN_CHARGING = r"""
+              ,,))))))));,
+           __)))))))))))))),
+\|/       -\(((((''''((((((((.
+-*-==//////((''  .     `)))))),
+/|\      ))| o    ;-.    '(((((                                  ,(,
+         ( `|    /  )    ;))))'                               ,_))^;(~
+            |   |   |   ,))((((_     _____------~~~-.        %,;(;(>';'~
+            o__;   ;    )))(((` ~---~  `::           \      %%~~)(v;(`('~
+                  ;    ''''````         `:       `:::|\,__,%%    );`'; ~
+                 |   _                )     /      `:|`----'     `-'
+           ______/\/~    |                 /        /
+         /~;;.____/;;'  /          ___--,-(   `;;;/
+        / //  _;______;'------~~~~~    /;;/\    /
+       //  | |                        / ;   \;;,\
+      (<_  | ;                      /',/-----'  _>
+       \_| ||_                     //~;~~~~~~~~~
+           `\_|                   (,~~
+                                   \~\
+                                    ~~
+"""
+
+# Sourced via https://www.asciiart.eu/art/4ac4be583e483ed5, author Colin J. Randall (CJRandall), modified
+UNICORN_REELING = r"""
+              \
+               \
+                \\
+                 \\
+                  >\`-_
+              _.-(6'  \\
+             (=___._/`'\\
+                  )  \ `|)
+                 /   /  '\)
+               j    < _  `-\
+           _.-' :      ``.  `
+           \ r=._\        `.
+          <`\\_  \         .`-.
+           \ r-7  `-. ._  ' .  `\
+            \`,      `-.`7  7)   )
+             \/         \|  \'  / `-._
+                        ||    .'
+                         \\  (
+                          >\  >
+                      ,.-' >.'
+                     <.'_.''
+                       <'
+"""
+
+# Combat outcomes dictionary
+combat_events = {
+    # --- Clashes ---
+    ("s", "s"): {
+        "lines": [
+            ("Both you and the beast lunge viciously at each other, your blade meeting its horn."),
+            ("Neither you nor the beast can overcome the other's strength and instead prepare the next attack."),
+        ],
+        "result": "clash",
+    },
+
+    ("b", "b"): {
+        "lines": [
+            ("You and the beast both brace yourselves for attack, only to be met by an odd stillness."),
+            ("When neither side moves, you loosen your grip on your shield and ready for the next opening."),
+        ],
+        "result": "clash",
+    },
+
+    ("f", "f"): {
+        "lines": [
+            ("You flinch as your false attack is seemingly met with an attack from the beast!"),
+            ("At the last second, it pulls off its attack, for a moment seeming just as surprised as you."),
+            ("Your mutual astonishment quickly fades and you ready yourself for the next window to attack."),
+        ],
+        "result": "clash",
+    },
+
+    # --- Player Advantage ---
+    ("s", "f"): {
+        "lines": [
+            ("The beast rears back in preparation for a larger strike, but your sword is already cutting through the air."),
+            ("The blade connects with flesh, staggering back the beast."),
+        ],
+        "result": "advantage",
+    },
+
+    ("b", "s"): {
+        "lines": [
+            ("The beast drives forward as you raise your shield."),
+            ("You dig your feet into the ground as the strike connects, holding until you're finally still."),
+            ("As soon as the beast's strike ends, you slam your shield forward, finding purchase against its face, granting you a precious window to strike."),
+        ],
+        "result": "advantage",
+    },
+
+    ("f", "b"): {
+        "lines": [
+            ("You swipe your blade to the right with half strength, the beast immediately moving to counter."),
+            ("As it readies to block, you redirect the strike with full force, finding purchase in the beast's flesh, staggering it back."),
+        ],
+        "pause": 1,
+        "result": "advantage",
+    },
+
+    # --- Player Disadvantage ---
+    ("s", "b"): {
+        "lines": [
+            ("Your entire body shifts as you put your full weight into your attack."),
+            ("With so much momentum already invested, you can only watch helplessly as the beast blocks your blow, taking advantage of your poor stance to counter-attack."),
+        ],
+        "result": "disadvantage",
+    },
+
+    ("b", "f"): {
+        "lines": [
+            ("The beast steps back as it readies a strong swing, prompting you to raise your shield."),
+            ("You stand there for a second, but when no attack slams against your shield, you peek your head over, only to see the true attack, now too late to stop."),
+        ],
+        "result": "disadvantage",
+    },
+
+    ("f", "s"): {
+        "lines": [
+            ("You swing your sword lazily, preparing to redirect the attack to avoid the beast's block, but the beast has no such intention."),
+            ("Your blade freezes as you feel its strike connect, only recovering fast enough to figure out how to escape worse consequences."),
+        ],
+        "result": "disadvantage",
+    },
+}
--- submenu.py
+++ submenu.py
@@ -1,194 +1,6 @@
 # Since I've decided to make the test available as an exe, I don't think I'm going to try to conceal the context anymore :)
 import time, random
-
-# Sourced via https://www.asciiart.eu/art/23319246a6009130, author Tim Spicer (Darkstar), modified
-CORRIDOR_SPLIT = r"""
- _____________________________________________
-|.'',                                     ,''.|
-|.'.'',                                 ,''.'.|
-|.'.'.'',                             ,''.'.'.|
-|.'.'.'.'',                         ,''.'.'.'.|
-|.'.'.'.'.|                         |.'.'.'.'.|
-|.'.'.'.'.|=========================|.'.'.'.'.|
-|.'.'.'.'.|:::|::::::::|::::::::|:::|.'.'.'.'.|
-|.'.'.'.'.|---|--------|--------|---|.'.'.'.'.|
-|.'.'.'.'.|:::|::::::::|::::::::|:::|.'.'.'.'.|
-|,',',',',|---|--------|--------|---|,',',',',|
-|.'.'.'.'.|:::|::::::::|::::::::|:::|.'.'.'.'.|
-|.'.'.'.'.|---|--------|--------|---|.'.'.'.'.|
-|.'.'.'.'.|=========================|.'.'.'.'.|
-|.'.'.'.'.|%%%%%%%%%%%%%%%%%%%%%%%%%|.'.'.'.'.|
-|.'.'.'.','       /%%%%%%%%%\       ','.'.'.'.|
-|.'.'.','        /%%%%%%%%%%%\        ','.'.'.|
-|.'.','         /%%%%%%%%%%%%%\         ','.'.|
-|.','          /%%%%%%%%%%%%%%%\          ','.|
-|;____________/%%%%%%%%%%%%%%%%%\____________;|
-"""
-
-# Sourced via https://www.asciiart.eu/art/619e9b9cbc49d3c1, author Shanaka Dias
-DRAGON_REELING = r"""
-             __                  __
-            ( _)                ( _)
-           / / \\              / /\_\_
-          / /   \\            / / | \ \
-         / /     \\          / /  |\ \ \
-        /  /   ,  \ ,       / /   /|  \ \
-       /  /    |\_ /|      / /   / \   \_\
-      /  /  |\/ _ '_| \   / /   /   \    \\
-     |  /   |/  0 \0\    / |    |    \    \\
-     |    |\|      \_\_ /  /    |     \    \\
-     |  | |/    \.\ o\o)  /      \     |    \\
-     \    |     /\\`v-v  /        |    |     \\
-      | \/    /_| \\_|  /         |    | \    \\
-      | |    /__/_ `-` /   _____  |    |  \    \\
-      \|    [__]  \_/  |_________  \   |   \    ()
-       /    [___] (    \         \  |\ |   |   //
-      |    [___]                  |\| \|   /  |/
-     /|    [____]                  \  |/\ / / ||
-    (  \   [____ /     ) _\      \  \    \| | ||
-     \  \  [_____|    / /     __/    \   / / //
-     |   \ [_____/   / /        \    |   \/ //
-     |   /  '----|   /=\____   _/    |   / //
-  __ /  /        |  /   ___/  _/\    \  | ||
- (/-(/-\)       /   \  (/\/\)/  |    /  | /
-               (/\/\)           /   /   //
-                      _________/   /    /
-                     \____________/    (
-"""
-
-# Sourced via https://www.asciiart.eu/art/376a59d7692b8079, author Shanaka Dias
-DRAGON_STATIC = r"""
- <>=======() 
-(/\___   /|\\          ()==========<>_
-      \_/ | \\        //|\   ______/ \)
-        \_|  \\      // | \_/
-          \|\/|\_   //  /\/
-           (oo)\ \_//  /
-          //_/\_\/ /  |
-         @@/  |=\  \  |
-              \_=\_ \ |
-                \==\ \|\_
-             __(\===\(  )\
-            (((~) __(_/   |
-                 (((~) \  /
-                 ______/ /
-                 '------'
-"""
-
-# Sourced via https://www.asciiart.eu/art/1833b5c2ffc7fafc, author Shanaka Dias, modified
-DRAGON_CHARGING = r"""
-          /                            )
-          (                             |\
-         /|                              \\
-        //                                \\
-       ///                                 \|
-      /( \                                  )\
-      \\  \_                               //)
-       \\  :\__                           ///
-        \\     )                         // \
-         \\:  /                         // |/
-          \\ / \                       //  \
-           /)   \   ___..-'           (|  \_|
-          //     /   _.'              \ \  \
-         /|       \ \________          \ | /
-        (| _ _  __/          '-.       ) /.'
-         \\ .  '-.__            \_    / / \
-          \\_'.     > --._ '.     \  / / /
-           \ \      \     \  \     .' /.'
-            \ \  '._ /     \ )    / .' |
-             \ \_     \_   |    .'_/ __/
-              \  \      \_ |   / /  _/ \_
-               \  \       / _.' /  /     \
-               \   |     /.'   / .'       '-,_
-                \   \  .'   _.'_/             \
-   /\    /\      ) ___(    /_.'           \    |
-  | _\__// \    (.'      _/               |    |
-  \/_  __  /--'`    ,                   __/    /
-  ( b) /b)  \  '.   :            \___.-'_/ \__/
-  /:/:  ,     ) :        (      /_.'__/-'|_ _ /
- /:/: __/\ >  __,_.----.__\    /        (/(/(/
-(_(,_/V .'/--'    _/  __/ |   /
- VvvV  //`    _.-' _.'     \   \
-   n_n//     (((/->/        |   /
-   '--'         ~='          \  |
-                              | |_,,,
-                              \  \  /
-                               '.__)
-"""
-
-# Sourced via https://www.asciiart.eu/art/627363361eda1a20, author unknown
-UNICORN_STATIC = r"""
-                              /
-                   __       //
-                   -\= \=\ //
-                 --=_\=---//=--
-               -_==/  \/ //\/--
-                ==/   /O   O\==--
-   _ _ _ _     /_/    \  ]  /--
-  /\ ( (- \    /       ] ] ]==-
- (\ _\_\_\-\__/     \  (,_,)--
-(\_/                 \     \-
-\/      /       (   ( \  ] /)
-/      (         \   \_ \./ )
-(       \         \      )  \
-(       /\_ _ _ _ /---/ /\_  \
- \     / \     / ____/ /   \  \
-  (   /   )   / /  /__ )   (  )
-  (  )   / __/ '---`       / /
-  \  /   \ \             _/ /
-  ] ]     )_\_         /__\/
-  /_\     ]___\
- (___)
-"""
-
-# Sourced via https://www.asciiart.eu/art/b73d333aa387ff07, author Tua Xiong, modified
-UNICORN_CHARGING = r"""
-              ,,))))))));,
-           __)))))))))))))),
-\|/       -\(((((''''((((((((.
--*-==//////((''  .     `)))))),
-/|\      ))| o    ;-.    '(((((                                  ,(,
-         ( `|    /  )    ;))))'                               ,_))^;(~
-            |   |   |   ,))((((_     _____------~~~-.        %,;(;(>';'~
-            o__;   ;    )))(((` ~---~  `::           \      %%~~)(v;(`('~
-                  ;    ''''````         `:       `:::|\,__,%%    );`'; ~
-                 |   _                )     /      `:|`----'     `-'
-           ______/\/~    |                 /        /
-         /~;;.____/;;'  /          ___--,-(   `;;;/
-        / //  _;______;'------~~~~~    /;;/\    /
-       //  | |                        / ;   \;;,\
-      (<_  | ;                      /',/-----'  _>
-       \_| ||_                     //~;~~~~~~~~~
-           `\_|                   (,~~
-                                   \~\
-                                    ~~
-"""
-
-# Sourced via https://www.asciiart.eu/art/4ac4be583e483ed5, author Colin J. Randall (CJRandall), modified
-UNICORN_REELING = r"""
-              \
-               \
-                \\
-                 \\
-                  >\`-_
-              _.-(6'  \\
-             (=___._/`'\\
-                  )  \ `|)
-                 /   /  '\)
-               j    < _  `-\
-           _.-' :      ``.  `
-           \ r=._\        `.
-          <`\\_  \         .`-.
-           \ r-7  `-. ._  ' .  `\
-            \`,      `-.`7  7)   )
-             \/         \|  \'  / `-._
-                        ||    .'
-                         \\  (
-                          >\  >
-                      ,.-' >.'
-                     <.'_.''
-                       <'
-"""
+import REFERENCE
 
 LINE_BREAK = "──────────────────────────────────────────────────────────"
 
@@ -199,11 +11,11 @@ def Minigame(frog_name="Testing Frog"):
     slow_print("The walls are decrepit and detritus crumbles from them when touched.")
     slow_print("You hear distant sounds of crackling. Fire, falling stone, or something more sinister.")
     slow_print("The moisture in the air is so thick it coats your skin.")
-    pause()
-    print(CORRIDOR_SPLIT)
+    pause(secs=1.5)
+    print(REFERENCE.CORRIDOR_SPLIT)
     slow_print("You begin forward into the dungeon, watching the light of outside fade behind you.")
-    slow_print("The stone walls begin to become uniform as press on, occluded from the elements. Ahead, the path splits, both ways looking identical.")
-    print("Only enter the letter corresponding to an action, as indicated by the parentheses. E.g. for (r)ight, enter 'r'.")
+    slow_print("The stone walls begin to become uniform as press you on, occluded from the elements. Ahead, the path splits, both ways looking identical.")
+    print("\nOnly enter the letter corresponding to an action, as indicated by the parentheses. E.g. for (r)ight, enter 'r'.")
 
     # Doesn't actually matter, but might as well make players feel like it does
     while True:
@@ -223,7 +35,7 @@ def Minigame(frog_name="Testing Frog"):
     # Fancy formatting
     slow_print("You snap out of your trance ", newline=False)
     slow_print("just in time", delay=0.005, newline=False)
-    pause(secs=1)
+    pause()
     slow_print(" barely dodging the unicorn's strike!")
     pause()
     slow_print("\nYou draw your sword and ready your stance. It's you or it.")
@@ -232,7 +44,7 @@ def Minigame(frog_name="Testing Frog"):
     Fight_One(frog_name)
 
 def Fight_One(frog_name):
-    Combat(static=UNICORN_STATIC, charging=UNICORN_CHARGING, reeling=UNICORN_REELING, player_health_max=PLAYER_START_HEALTH, enemy_health_max=6, enemy_name="Unicorn")
+    Combat(static=REFERENCE.UNICORN_STATIC, charging=REFERENCE.UNICORN_CHARGING, reeling=REFERENCE.UNICORN_REELING, player_health_max=PLAYER_START_HEALTH, enemy_health_max=6, enemy_name="Unicorn")
 
 def Combat(static="S", charging="C", reeling="R", player_health_max=10, enemy_health_max=6, enemy_name="Testing Monster"):
     """
@@ -263,65 +75,7 @@ def Combat(static="S", charging="C", reeling="R", player_health_max=10, enemy_he
         enemy_action = Random_Static_Attack()
 
         # TO DO: make dialog variable-based instead of hard coding!
-        # This is probably better suited for like a dictionary or something, but the performance loss is so small that readability still wins
-        if player_action == enemy_action:
-            # "Clashes" where both characters pick the same option, no outcome
-            if player_action == "s":
-                combat_print("Both you and the beast lunge viciously at each other, your blade meeting its horn.")
-                combat_print("Neither you nor the beast can overcome the other's strength and instead prepare the the next attack.")
-                pause()
-            elif player_action == "b":
-                combat_print("You and the beast both brace yourselves for attack, only to be met by an odd stillness.")
-                combat_print("When neither side moves, you loosen your grip on your shield and ready for the next opening.")
-                pause()
-            elif player_action == "f":
-                # Slightly faster dialog since there's more of it
-                combat_print("You flinch as your false attack is seemingly met with an attack from beast!", delay=0.017)
-                pause(0.5)
-                combat_print("At the last second, it pulls off its attack, for a moment seeming just as surprised as you.", delay=0.017)
-                combat_print("Your mutual astonishment quickly fades and you ready yourself for the next window to attack.", delay=0.017)
-                pause(0.5) # Reduced pause for double pauses
-        # -- Player Advantage --
-        elif player_action == "s" and enemy_action == "f":
-            combat_print("The beast rears back in preperation for a larger strike, but your sword is already cutting through the air.")
-            combat_print("The blade connects with flesh, staggering back the beast.")
-            pause()
-            # Advantage Menu
-        elif player_action == "b" and enemy_action == "s":
-            combat_print("The beast drives forward as you raise your shield.", delay=0.017)
-            combat_print("You dig your feet into the ground as the strike connects, holding until you're finally still.", delay=0.017)
-            combat_print("As soon as the beast's strike ends, you slam your shield forward, finding purchase against its face, granting you a precious window to strike.", delay=0.017)
-            pause()
-            # Advantage Menu
-        elif player_action == "f" and enemy_action == "b":
-            combat_print("You swipe your blade to the right with half strength, the beast immediately moving to counter.")
-            combat_print("As it readies to block, you redirect the strike with full force, finding purchase in the beast's flesh, staggering it back.")
-            pause()
-            # Advantage Menu
-        # -- Player Disadvantage --
-        elif player_action == "s" and enemy_action == "b":
-            combat_print("Your entire body shifts as you put your full weight into your attack.")
-            combat_print("With so much momentum already invested, you can only watch helplessly as the beast blocks your blow, taking advantage of your poor stance to counter-attack.")
-            pause()
-            # Disadvantage Menu
-        elif player_action == "b" and enemy_action == "f":
-            combat_print("The beast steps back as it readies a strong swing, prompting you to raise your shield.")
-            pause(0.5)
-            combat_print("You stand there for a second, but when no attack slams against your shield, you peek your head over, only to see the true attack, now too late to stop.")
-            pause(0.5)
-            # Disadvantage Menu
-        elif player_action == "f" and enemy_action == "s":
-            combat_print("You swing your sword lazily, preparing to redirect the attack to avoid the beast's block, but the beast has no such intention.")
-            combat_print("Your blade freezes as you feel its strike connect, only recovering fast enough to figure out how to escape worse consequences.")
-            pause()
-            # Disadvantage Menu
-        # -- Error Case --
-        else:
-            slow_print("Something about your action caused an error.")
-            pause()
-            slow_print("Maybe something to discuss with the developer?")
-            slow_print("Try again, but do something differently this time.")
-            pause()
+        run_combat_event(player_action, enemy_action)
 
 # Display health and options, return valid answer
 def Static_Menu(enemy_name, enemy_health_current, enemy_health_max, player_health_current, player_health_max, display_hint=False):
@@ -353,6 +107,26 @@ def Random_Static_Attack():
     if r == 3:
         return "f"
 
+# Runs the dictionary and returns the outcome
+# Returns "clash", "advantage", or "disadvantage"
+def run_combat_event(player_action, enemy_action):
+    event = REFERENCE.combat_events.get((player_action, enemy_action))
+
+    if not event:
+        slow_print("Something about your action caused an error.")
+        pause()
+        slow_print("Maybe something to discuss with the developer?")
+        slow_print("Try again, but do something differently this time.")
+        pause()
+        return
+
+    for line in event["lines"]:
+        combat_print(line)
+
+    pause(secs=1.5)
+
+    return event.get("result")
+
 def slow_print(text, delay=0.027, newline=True):
     for ch in text: # I keep forgetting you don't need parentheses in python
         print(ch, end='', flush=True) # Flush needs to be true or messes up
@@ -364,7 +138,7 @@ def slow_print(text, delay=0.027, newline=True):
 def combat_print(text, delay=0.02, newline=True):
     slow_print(text, delay=delay, newline=newline)
 
-def pause(secs=0.7):
+def pause(secs=1):
     time.sleep(secs)
 
 # Makes things look a little cleaner
